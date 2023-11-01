3576fbdfc1ac9cd15adee12f6164c315deb8b359
dat.truong <dat.truong@secomm.vn>
2023-11-01 09:09:18 +0700

    radio group

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
-// import { MuiTypography } from "./components/MuiTypography";
-// import MuiButton from "./components/MuiButton";
+import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiSelect from "./components/MuiSelect";
 // import MuiTextField from "./components/MuiTextField";
-import MuiSelect from "./components/MuiSelect";
+// import MuiButton from "./components/MuiButton";
+// import { MuiTypography } from "./components/MuiTypography";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      <MuiRadioButton />
     </div>
   );
 }
---
+++ src/components/MuiRadioButton.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormHelperText,
+} from "@mui/material";
+
+const MuiRadioButton = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  console.log({ value });
+
+  return (
+    <Box width={250}>
+      <FormControl error>
+        <FormLabel id="job-experience-group-label">
+          Years of experience
+        </FormLabel>
+        <RadioGroup
+          name="job-experience-group"
+          aria-labelledby="job-experience-group-label"
+          value={value}
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            control={<Radio size="medium" color="secondary" />}
+            label="0-2"
+            value="0-2"
+          />
+          <FormControlLabel control={<Radio />} label="3-5" value="3-5" />
+          <FormControlLabel control={<Radio />} label="6-10" value="6-10" />
+        </RadioGroup>
+        <FormHelperText>Invalid Selection</FormHelperText>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default MuiRadioButton;
